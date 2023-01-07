ee7c8416c57e4b96c029d75d740d4ec8735dfae3
Jonathan Barsky <barskyjonathan96@gmail.com>
2023-01-07 13:44:42 -0300

    separe en modulos el js con inports dinámicos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-const modal = document.querySelector(".modal");
-const button1 = document.querySelector("#button1");
-const button2 = document.querySelector("#button2");
-const button3 = document.querySelector("#button3");
-const closeButton = document.querySelector(".modal_content-close");
+const buttonOpenModal = document.querySelectorAll(".footer_button");
 const onViewPort = document.querySelectorAll(".onViewPort");
+const closeButton = document.querySelector(".modal_content-close");
 
-function openModal(){
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-    modal.classList.remove("hidden");
-    modal.classList.add("visible");
-    modal.classList.add("appear");
-}
-button1.addEventListener("click", () => openModal())
-button2.addEventListener("click", () => openModal())
-button3.addEventListener("click", () => openModal())
+buttonOpenModal.forEach((item => {
+    item.addEventListener("click", async () => {
+        const module = await import('./toggleModal.js')
+        module.openModal();
+    })
+}))
 
-closeButton.addEventListener("click", () => {
-    modal.classList.remove("appear");
-    modal.classList.add("disappear");
-    setTimeout(() => {
-        modal.classList.remove("visible")
-        modal.classList.add("hidden")
-        modal.classList.remove("disappear");
-    }, 2500)
+closeButton.addEventListener("click", async () => {
+    const modal = await import('./toggleModal.js')
+    modal.closeModal();
 })
 
 const observer = new IntersectionObserver((entries) => {
---
+++ src/toggleModal.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal");
+
+function openModal(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+    modal.classList.add("appear");
+}
+
+function closeModal(){
+    modal.classList.remove("appear");
+    modal.classList.add("disappear");
+    setTimeout(() => {
+        modal.classList.remove("visible")
+        modal.classList.add("hidden")
+        modal.classList.remove("disappear");
+    }, 2500)
+}
+export {openModal, closeModal};
