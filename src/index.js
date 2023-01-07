bfcb2430afea6378878e18cb25f124e9517c3ecf
Jonathan Barsky <barskyjonathan96@gmail.com>
2023-01-07 12:34:47 -0300

    agregue la etiqueta picture con imagenes chicas y normales dependiendo del tamaño de pantalla, animaciones al hacer scroll, efectos para el modal al activarse y desactivarse

--- src/index.js
+++ src/index.js
@@ -3,17 +3,36 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const closeButton = document.querySelector(".modal_content-close");
+const onViewPort = document.querySelectorAll(".onViewPort");
 
 function openModal(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
     modal.classList.remove("hidden");
     modal.classList.add("visible");
+    modal.classList.add("appear");
 }
 button1.addEventListener("click", () => openModal())
 button2.addEventListener("click", () => openModal())
 button3.addEventListener("click", () => openModal())
 
 closeButton.addEventListener("click", () => {
-    console.log("here")
-    modal.classList.remove("visible")
-    modal.classList.add("hidden")
+    modal.classList.remove("appear");
+    modal.classList.add("disappear");
+    setTimeout(() => {
+        modal.classList.remove("visible")
+        modal.classList.add("hidden")
+        modal.classList.remove("disappear");
+    }, 2500)
+})
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("appear")
+        }  
+    })    
+})
+onViewPort.forEach((item) =>{
+    observer.observe(item);
 })
