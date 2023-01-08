f2a900e8e9d66f6db640c1ce53244a02f0be9f9a
Jonathan Barsky <barskyjonathan96@gmail.com>
2023-01-07 21:14:18 -0300

    modifique el intersection observer

--- src/index.js
+++ src/index.js
@@ -14,13 +14,18 @@ closeButton.addEventListener("click", async () => {
     modal.closeModal();
 })
 
+let options = {
+    root: null,
+    rootMargin: "0px -20px 0px 0px",
+    threshold: 0
+}
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
             entry.target.classList.add("appear")
         }  
     })    
-})
+}, options)
 onViewPort.forEach((item) =>{
     observer.observe(item);
 })
